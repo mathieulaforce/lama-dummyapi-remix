e581ed8ce17f7cf00cf289ea6cbe0dd6c9814ffb
Mathieu Laforce <mathieulaforce@hotmail.com>
2022-11-27 17:46:08 +0100

    add home route

---
+++ code/lama-shop/app/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+ 
+import {  Container} from '@mui/material';
+
+ 
+
+const ProductsPage: React.FC = () => { 
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+};
+
+export default ProductsPage;
